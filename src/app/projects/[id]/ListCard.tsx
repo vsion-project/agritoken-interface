d99c25b618a9ca17d00e004da2189481c1342b97
RavilcoDev <r.livise@hotmail.com>
2023-08-29 20:17:26 -0500

    feat: home and projects

---
+++ src/app/projects/[id]/ListCard.tsx
@@ -0,0 +1,50 @@
+'use client'
+import CardBuyNFT from './CardBuyNFT';
+import { TPropsCardBuyNFT } from '@/app/types';
+import React from 'react';
+
+const deposits: TPropsCardBuyNFT[] = [
+  {
+    id: '3',
+    title: 'NFT 1/4 HA',
+    image: '/token/post_corn_1_4.png',
+    description: 'Equivale una hectarea de cultivo de maiz ',
+    price: 500
+  },
+  {
+    id: '2',
+    title: 'NFT 1/2 HA',
+    image: '/token/post_corn_1_2.png',
+    description: 'Equivale media hectarea de cultivo de maiz ',
+    price: 1000
+  },
+
+  {
+    id: '1',
+    title: 'NFT 1 HA',
+    image: '/token/post_corn_1.png',
+    description: 'Equivale un cuarto de hectarea de cultivo de maiz ',
+    price: 2000
+  },
+]
+
+
+const CardList = ({ id }: { id: string }) => {
+  return (
+    <>
+      {id == '1'
+        ? (
+          <div className='w-full grid grid-flow-row gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+            {deposits.map((dep, idx) => (<CardBuyNFT key={idx} {...dep} />))}
+          </div>)
+        : (
+          <h3>
+            No existe este projecto
+          </h3>
+        )
+      }
+    </>
+  );
+};
+
+export default CardList;
